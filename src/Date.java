7bf8d198e3610046a3a14d2668207745e664c104
oded-kapeta <oded.kapeta@campus.technion.ac.il>
2023-05-31 19:24:01 +0300

    almost finish part A

--- src/Date.java
+++ src/Date.java
@@ -1,2 +1,64 @@
 public class Date {
+    public int day = 1;
+    public int month = 1;
+    public int year = 0;
+
+    public Date(int myyear,int mymonth,int myday){
+        setDay(myday);
+        setYear(myyear);
+        setMonth(mymonth);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)    return true;
+        if (obj == null)    return false;
+        return (this.hashCode() == obj.hashCode());
+
+    }
+
+    @Override
+    public String toString() {
+        String dayString = String.valueOf(this.day);
+        String monthString = String.valueOf(this.month);
+        String yearString = String.valueOf(this.year);
+        int lengthDay = dayString.length();
+        int lengthMonth = monthString.length();
+        int lengthYear = yearString.length();
+        while (lengthYear<4 || lengthDay<2 || lengthMonth<2){
+            if (lengthYear < 4){
+                System.out.println(yearString);
+                yearString = "0"+yearString;
+                System.out.println(yearString);
+                lengthYear++;
+            }
+            if (lengthMonth < 2){
+                monthString = "0"+monthString;
+                lengthMonth++;
+            }
+            if (lengthDay < 2){
+                dayString = "0"+dayString;
+                lengthDay++;
+            }
+        }
+        String finalString = dayString + "/" + monthString + "/" + yearString;
+        return finalString;
+    }
+
+    @Override
+    public int hashCode() {
+        return 3999*this.year + 31*this.month + 12*this.year;
+    }
+
+    public void setDay(int day) {
+        if (day >=1 && day<=31) this.day = day;
+    }
+
+    public void setMonth(int month) {
+        if(month <=12 && month>=1)  this.month = month;
+    }
+
+    public void setYear(int year) {
+        if(year<=3999 && year>=-3999)   this.year = year;
+    }
 }
--- src/DateTime.java
+++ src/DateTime.java
@@ -1,2 +1,52 @@
-public class DateTime {
+public class DateTime extends Date {
+    private int minute = 0;
+    private int hour = 0;
+    public DateTime(int myday,int mymonth,int myyear,int myminute,int myhour){
+        super(myyear,mymonth,myday);
+        setHour(myhour);
+        setMinute(myminute);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)    return true;
+        if (obj == null || !(obj instanceof DateTime))    return false;
+        DateTime newobj = (DateTime) obj;
+        return super.day==newobj.day && super.month== newobj.month && super.year==newobj.year && this.hour==newobj.hour && this.minute==newobj.minute ;
+    }
+
+    @Override
+    public String toString() {
+        String superToString = super.toString();
+        String minuteString = String.valueOf(this.minute);
+        String hourString = String.valueOf(this.hour);
+        int lengthMinute = minuteString.length();
+        int lengthHour = hourString.length();
+        while (lengthHour <2 || lengthMinute <2){
+            if (lengthHour < 2){
+                hourString = "0"+hourString;
+                lengthHour++;
+            }
+            if (lengthMinute < 2){
+                minuteString = "0"+minuteString;
+                lengthMinute++;
+            }
+        }
+        String finalString = superToString + " " + hourString + ":" + minuteString;
+        return finalString;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return 3999*this.year + 31*this.month + 12*this.year + 23*this.hour + 59*this.minute;
+    }
+
+    public void setMinute(int minute) {
+        if(minute<=59 && minute>=0) this.minute = minute;
+    }
+
+    public void setHour(int hour) {
+        if(hour<=23 && hour>=0) this.hour = hour;
+    }
 }
